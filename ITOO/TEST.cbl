6027d1115e24b9e67b4fee152e4ad384d0910fe3
Zawng <edwinjpr11@gmail.com>
2022-05-20 14:40:02 -0500

    chore: update script, add validations

--- ITOO/TEST.cbl
+++ ITOO/TEST.cbl
@@ -1,16 +1,17 @@
       ******************************************************************
-      * Author: e
-      * Date:
-      * Purpose:
+      * Author: Edwin Páez
+      * Date: 20-05-22
+      * Purpose: Practice COBOL
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. TEST.
+       AUTHOR.     EDWIN.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-            DISPLAY "Hello world"
+            DISPLAY "Hello world".
             STOP RUN.
        END PROGRAM TEST.
