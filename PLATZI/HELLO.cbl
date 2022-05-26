37be8d367cb931a543c55cee52014301ed53ba64
Zawng <edwinjpr11@gmail.com>
2022-05-26 12:21:31 -0500

    chore: delete trash

--- PLATZI/HELLO.cbl
+++
@@ -1,8 +0,0 @@
-      * Esto es un comentario en COBOL 
-       IDENTIFICATION DIVISION.             
-       PROGRAM-ID.                     HOLA.
-       ENVIRONMENT DIVISION.                
-       DATA DIVISION.                       
-       PROCEDURE DIVISION.                  
-           DISPLAY 'HOLA, MUNDO'.           
-           STOP RUN.                        
