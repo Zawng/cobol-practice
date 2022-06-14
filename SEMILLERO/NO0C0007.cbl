f4b9dd27f858e7ab85fe78bc7a3ebf4e30dbb644
Zawng <edwinjpr11@gmail.com>
2022-06-14 07:07:22 -0500

    chore: add new practice

--- SEMILLERO/NO0C0007.cbl
+++ SEMILLERO/NO0C0007.cbl
@@ -2,28 +2,13 @@
       ******************************************************************
       * Author: EDWIN PAEZ                                             *
       * Purpose: PRACTICE COBOL                                        *
-      * TODO:                                                          *
-      * ENTRADA:
-      * [ ] RECIBIR DOS NUMEROS
-      *     DIGITE UN NUMERO POSITIVO (12ENT).(2DEC)
-      *     DIGITE UN NUMERO NEGATIVO (12ENT).(2DEC)
-      * SALIDA:
-      * [ ] NUMERO POSITIVO: MOSTRAR NUMERO TAL CUAL
-      * [ ] NUMERO NEGATIVO: MOSTRAR NUMERO TAL CUAL
-      * [ ] 1) MÁSCARA
-      * [ ] 2) MÁSCARA
-      * [ ] 3) MÁSCARA
-      * [ ] 4) MÁSCARA
-      * [ ] 5) MÁSCARA
-      * [ ] 6) MÁSCARA
-      * [ ] 7) MÁSCARA SIGNOS
       ******************************************************************
 
       *----------------------------------------------------------------*
       *                           IDENTIFICATION                       *
       *----------------------------------------------------------------*
        ID DIVISION.
-       PROGRAM-ID.                             NO0C0004.
+       PROGRAM-ID.                             NO0C0007.
        AUTHOR.                                 NOVATEC (EDWIN-PAEZ).
        INSTALLATION.                           BBVA.
        DATE-WRITTEN.                           13-JUN-22.
@@ -40,10 +25,10 @@
        WORKING-STORAGE SECTION.
        01  WS-VARIABLES.
            02 WS-ENTER   PIC X(01)       VALUE SPACES.
-           02 WS-NUM-01  PIC 9(12)V9(2)  VALUE 000456789123.45.
-           02 WS-NUM-02  PIC -9(12)V9(2) VALUE -000456789123.45.
+           02 WS-NUM-01  PIC 9(12)V9(2)  VALUE ZEROES.
+           02 WS-NUM-02  PIC -9(12)V9(2) VALUE ZEROES.
       *----------------------------------------------------------------*
-      * MÁSCARAS POSITIVOS
+      * MASCARAS POSITIVOS
       *----------------------------------------------------------------*
            02 WS-MP-01   PIC 9(12).9(02).
            02 WS-MP-02   PIC 9(03),9(03),9(03),9(03).9(02).
@@ -54,13 +39,13 @@
            02 WS-MP-07   PIC +(12).9(02).
 
       *----------------------------------------------------------------*
-      * MÁSCARAS NEGATIVOS
+      * MASCARAS NEGATIVOS
       *----------------------------------------------------------------*
-           02 WS-MN-01   PIC -*(11)9.9(02).
-           02 WS-MN-02   PIC -9(12).9(02).
-           02 WS-MN-03   PIC -/(12).9(02).
+           02 WS-MN-01   PIC $(11)9.9(02).
+           02 WS-MN-02   PIC +(12).9(02).
+           02 WS-MN-03   PIC /(11)9.9(02).
            02 WS-MN-04   PIC Z(12).9(02)DB.
-           02 WS-MN-05   PIC -*(03),*(03),*(03),*(03).9(02).
+           02 WS-MN-05   PIC *(03),*(03),*(03),*(03).9(02).
            02 WS-MN-06   PIC *(11)9.9(02)CR.
            02 WS-MN-07   PIC -(12).9(02).
 
@@ -93,7 +78,7 @@
        PROCEDURE DIVISION.
        1000-PRINCIPAL.
        PERFORM 2001-FECHAS
-      *>  PERFORM 2002-INGRESAR-INFORMACION
+       PERFORM 2002-INGRESAR-INFORMACION
        PERFORM 2005-MOSTRAR-M-POSITIVAS
        PERFORM 2006-MOSTRAR-M-NEGATIVAS
        PERFORM 3000-FINAL.
@@ -113,9 +98,9 @@
 
        2001-PANTALLA-FECHAS.
            DISPLAY 'FECHA DEL SISTEMA: '       LINE 01 POSITION 01
-           DISPLAY WS-FECHA-SIS                LINE 01 POSITION 20          10
+           DISPLAY WS-FECHA-SIS                LINE 01 POSITION 20
            DISPLAY 'HORA DEL SISTEMA: '        LINE 01 POSITION 54
-           DISPLAY WS-HORA-SIS                 LINE 01 POSITION 72.         08
+           DISPLAY WS-HORA-SIS                 LINE 01 POSITION 72.
 
        2002-INGRESAR-INFORMACION.
            PERFORM 2001-PANTALLA-FECHAS
@@ -138,49 +123,46 @@
 
        2005-MOSTRAR-M-POSITIVAS.
            DISPLAY CLEAR-SCREEN
-           PERFORM 2003-MASCARAS-POSITIVAS
            PERFORM 2001-PANTALLA-FECHAS
-           DISPLAY 'NUMERO POSITIVO :'         LINE 08 POSITION 01
-           DISPLAY WS-NUM-01                   LINE 08 POSITION 19
-           DISPLAY 'PIC 9(12).9(02) :'         LINE 10 POSITION 01
-           DISPLAY WS-MP-01                    LINE 10 POSITION 38
-           DISPLAY 'PIC 9(03),9(03),9(03),9(03).9(02) :'
-                                               LINE 11 POSITION 01
-           DISPLAY WS-MP-02                    LINE 11 POSITION 38
-           DISPLAY 'PIC Z(11)9.9(02) :'        LINE 12 POSITION 01
-           DISPLAY WS-MP-03                    LINE 12 POSITION 38
-           DISPLAY 'PIC *(11)9.9(02) :'        LINE 13 POSITION 01
-           DISPLAY WS-MP-04                    LINE 13 POSITION 38
-           DISPLAY 'PIC 9(03)/9(03)/9(03)/9(03).9(02) :' 
-                                               LINE 14 POSITION 01
-           DISPLAY WS-MP-05                    LINE 14 POSITION 38
-           DISPLAY 'PIC *(11)9.9(02)CR :'      LINE 15 POSITION 01
-           DISPLAY WS-MP-06                    LINE 15 POSITION 38
-           DISPLAY 'PIC +(12).9(02 :'          LINE 16 POSITION 01
-           DISPLAY WS-MP-07                    LINE 16 POSITION 38
-           PERFORM 2007-ENTER.
+           PERFORM 2003-MASCARAS-POSITIVAS
+           PERFORM 2004-MASCARAS-NEGATIVAS
+           DISPLAY 'NUMERO POSITIVO :'         LINE 03 POSITION 01
+           DISPLAY WS-NUM-01                   LINE 03 POSITION 19
+           DISPLAY '1) 9(12).9(02) :'          LINE 04 POSITION 01
+           DISPLAY WS-MP-01                    LINE 04 POSITION 38
+           DISPLAY '2) 9(03),9(03),9(03),9(03).9(02) :'
+                                               LINE 05 POSITION 01
+           DISPLAY WS-MP-02                    LINE 05 POSITION 38
+           DISPLAY '3) Z(11)9.9(02) :'         LINE 06 POSITION 01
+           DISPLAY WS-MP-03                    LINE 06 POSITION 38
+           DISPLAY '4) *(11)9.9(02) :'         LINE 07 POSITION 01
+           DISPLAY WS-MP-04                    LINE 07 POSITION 38
+           DISPLAY '5) 9(03)/9(03)/9(03)/9(03).9(02) :'
+                                               LINE 08 POSITION 01
+           DISPLAY WS-MP-05                    LINE 08 POSITION 38
+           DISPLAY '6) *(11)9.9(02)CR :'       LINE 09 POSITION 01
+           DISPLAY WS-MP-06                    LINE 09 POSITION 38
+           DISPLAY '7) +(12).9(02 :'           LINE 10 POSITION 01
+           DISPLAY WS-MP-07                    LINE 10 POSITION 38.
 
        2006-MOSTRAR-M-NEGATIVAS.
-           DISPLAY CLEAR-SCREEN
-           PERFORM 2004-MASCARAS-NEGATIVAS
-           PERFORM 2001-PANTALLA-FECHAS
-           DISPLAY 'NUMERO NEGATIVO :'         LINE 08 POSITION 01
-           DISPLAY WS-NUM-02                   LINE 08 POSITION 19
-           DISPLAY 'PIC -*(11)9.9(02): '       LINE 10 POSITION 01
-           DISPLAY WS-MN-01                    LINE 10 POSITION 38
-           DISPLAY 'PIC -9(12).9(02) :'        LINE 11 POSITION 01
-           DISPLAY WS-MN-02                    LINE 11 POSITION 38
-           DISPLAY 'PIC -/(12).9(02) :'        LINE 12 POSITION 01
-           DISPLAY WS-MN-03                    LINE 12 POSITION 38
-           DISPLAY 'PIC Z(12).9(02)DB :'       LINE 13 POSITION 01
-           DISPLAY WS-MN-04                    LINE 13 POSITION 38
-           DISPLAY 'PIC -*(03),*(03),*(03),*(03).9(02) :'  
-                                               LINE 14 POSITION 01
-           DISPLAY WS-MN-05                    LINE 14 POSITION 38
-           DISPLAY 'PIC *(11)9.9(02)CR :'      LINE 15 POSITION 01
-           DISPLAY WS-MN-06                    LINE 15 POSITION 38
-           DISPLAY 'PIC -(12).9(02) :'         LINE 16 POSITION 01
-           DISPLAY WS-MN-07                    LINE 16 POSITION 38
+           DISPLAY 'NUMERO NEGATIVO :'         LINE 12 POSITION 01
+           DISPLAY WS-NUM-02                   LINE 12 POSITION 19
+           DISPLAY '1) *(11)9.9(02): '         LINE 13 POSITION 01
+           DISPLAY WS-MN-01                    LINE 13 POSITION 38
+           DISPLAY '2) +(12).9(02) :'          LINE 14 POSITION 01
+           DISPLAY WS-MN-02                    LINE 14 POSITION 38
+           DISPLAY '3) /(11)9.9(02) :'         LINE 15 POSITION 01
+           DISPLAY WS-MN-03                    LINE 15 POSITION 38
+           DISPLAY '4) Z(12).9(02)DB :'        LINE 16 POSITION 01
+           DISPLAY WS-MN-04                    LINE 16 POSITION 38
+           DISPLAY '5) *(03),*(03),*(03),*(03).9(02) :'
+                                               LINE 17 POSITION 01
+           DISPLAY WS-MN-05                    LINE 17 POSITION 38
+           DISPLAY '6) *(11)9.9(02)CR :'       LINE 18 POSITION 01
+           DISPLAY WS-MN-06                    LINE 18 POSITION 38
+           DISPLAY '7) -(12).9(02) :'          LINE 19 POSITION 01
+           DISPLAY WS-MN-07                    LINE 19 POSITION 38
            PERFORM 2007-ENTER.
 
        2007-ENTER.
