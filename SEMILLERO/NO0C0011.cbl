d2d68ae7b39895667018522d4e401c9447432a8b
Zawng <edwinjpr11@gmail.com>
2022-06-29 14:47:13 -0500

    chore: end project with if else sentences

--- SEMILLERO/NO0C0011.cbl
+++ SEMILLERO/NO0C0011.cbl
@@ -1,26 +1,8 @@
       ******************************************************************
       * Author: EDWIN PAEZ                                             *
       * Purpose: PRACTICE COBOL                                        *
-      * TODO:                                                          *
-      * CALCULAR VALOR DE LA CUOTA QUE DEBE PAGAR UN CLIENTE DEPENDIENDO
-      * DEL TIPO DE PRODUCTO QUE SELECCIONE
-      * [PRODUCTOS] Interés efectivo anual
-      * 1. TARJETA DE CREDITO:       30%, 05 AÑOS
-      * 2. PRESTAMO HIPOTECARIO:     16%, 20 AÑOS
-      * 3. PRESTAMO VEHICULO:        18%, 06 AÑOS
-      * 4. PRESTAMO LIBRE INVERSION: 24%, 05 AÑOS
-      * 5. PRESTAMO PARA EDUCACION:  19%, 07 AÑOS
-      * SEGURO:                   1.5%
-
-      * [VALIDACIONES] HOMBRE / MUJER
-      * ¿CABEZA DE HOGAR?
-      * MUJER: -2% AL PRODUCTO
-      * HOMBRE: -1.5% AL PRODUCTO
-
-      * SALIDA:
-      * [] INTERES MENSUAL [X] SEGURO MENSUAL [] TOTAL MENSUAL
-      * [] TOTAL A PAGAR DURANTE EL TIEMPO
-      * INTERES, CAPITAL, SEGURO, total MENSUAL
+      * CALCULAR EL VALOR DE LA CUOTA QUE DEBE PAGAR UN CLIENTE        *
+      * DEPENDIENDO DEL TIPO DE PRODUCTO QUE SELECCIONE                *
       *----------------------------------------------------------------*
       *                           IDENTIFICATION                       *
       *----------------------------------------------------------------*
@@ -51,15 +33,8 @@
       * ANOS 
        01  WS-ANO-MAS                 PIC ZZ.
 
-       01  WS-SEG-TOT-MAS             PIC $$$,$$$,$$$,$$$,$$9.9(02). 
-       01  WS-CAPITAL-MAS             PIC $$$,$$$,$$$,$$$,$$9.9(02).
-       01  WS-CUOTAS-MAS              PIC $$$,$$$,$$$,$$$,$$9.9(02).
-       01  WS-MES-TOT-MAS             PIC $$$,$$$,$$$,$$$,$$9.9(02).
-       01  WS-MAS-TOT                 PIC $$$,$$$,$$$,$$$,$$9.9(02).
-       01  WS-SEGURO-MAS              PIC $$$,$$$,$$$,$$$,$$9.9(02).
-       01  WS-INTERES-MAS             PIC $$$,$$$,$$$,$$$,$$9.9(02).
+      * SEGURO 
        01  WS-MAS-SEG                 PIC 9.9.
-       01  WS-MAS-INT                 PIC Z9.9(02).
 
       *----------------------------------------------------------------*
       * TITULO PANTALLA
@@ -111,7 +86,6 @@
 
        01  WS-INTERES-PAN             PIC 99V99 VALUE ZEROS.
        01  WS-INTERES                 PIC 9V9(03) VALUE ZEROS.
-      * HOMBRE O MUJER NO CABEZA DE HOGAR 
            88 WS-INT-TDC              VALUE 0.300.
            88 WS-INT-HIP              VALUE 0.160.
            88 WS-INT-VEH              VALUE 0.180.
@@ -160,16 +134,17 @@
        1000-PRINCIPAL.
        PERFORM 2001-FECHAS
        PERFORM 2004-INFORMACION
-       PERFORM 2006-ACTIVAR-TASA-PRODUCTO.
+       PERFORM 2006-ACTIVAR-TASA-PRODUCTO
+       PERFORM 2009-HALLAR-SEXO
        PERFORM 2008-HALLAR-DESCUENTOS
        PERFORM 2007-HALLAR-INTERESES
-       PERFORM 2011-HALLAR-SEGURO-MENSUAL
-       PERFORM 2013-HALLAR-INTERESES-MENSUALES
-       PERFORM 2015-HALLAR-MENSUALES-TOTALES
-       PERFORM 2016-HALLAR-CAPITAL-MENSUAL
-       PERFORM 2017-HALLAR-CUOTA-MENSUAL
-       PERFORM 2014-HALLAR-TOTAL
-       PERFORM 2019-SALIDA
+       PERFORM 2010-HALLAR-SEGURO-MENSUAL
+       PERFORM 2011-HALLAR-INTERESES-MENSUALES
+       PERFORM 2013-HALLAR-MENSUALES-TOTALES
+       PERFORM 2014-HALLAR-CAPITAL-MENSUAL
+       PERFORM 2015-HALLAR-CUOTA-MENSUAL
+       PERFORM 2012-HALLAR-TOTAL
+       PERFORM 2016-SALIDA
        PERFORM 3000-FINAL.
 
       *----------------------------------------------------------------*
@@ -199,8 +174,16 @@
        2004-INFORMACION.
            PERFORM 2002-PANTALLA-FECHAS
            PERFORM 2003-BANNER
+           PERFORM 2005-CAPTURA-CAMPOS.
 
-      * TIPO DE PRODUCTO     
+       2005-CAPTURA-CAMPOS.
+           PERFORM 2005-01-CAPTURA-PRODUCTO
+           PERFORM 2005-02-CAPTURA-CAPITAL
+           PERFORM 2005-03-CAPTURA-TIEMPO
+           PERFORM 2005-04-CAPTURA-SEXO
+           PERFORM 2005-05-CAPTURA-HOGAR.
+
+       2005-01-CAPTURA-PRODUCTO.
            DISPLAY 'SELECCIONE UN PRODUCTO:'
                                          LINE 07 POSITION 01
            DISPLAY '1) TARJETA DE CREDITO       - 30%, 05 ANOS'
@@ -214,18 +197,18 @@
            DISPLAY '5) PRESTAMO PARA EDUCACION  - 19%, 07 ANOS'
                                          LINE 12 POSITION 01
            DISPLAY 'OPCION) '            LINE 13 POSITION 01
-           ACCEPT WS-PRODUCTO            LINE 13 POSITION 09
+           ACCEPT WS-PRODUCTO            LINE 13 POSITION 09.
 
-      * CAPITAL
-           DISPLAY 'INGRESE EL CAPITAL:' LINE 16 POSITION 01
-           ACCEPT WS-CAPITAL             LINE 16 POSITION 21
-           DIVIDE 100 INTO WS-CAPITAL    END-DIVIDE
+       2005-02-CAPTURA-CAPITAL.
+           DISPLAY 'INGRESE EL CAPITAL:' LINE 15 POSITION 01
+           ACCEPT WS-CAPITAL             LINE 15 POSITION 21
+           DIVIDE 100 INTO WS-CAPITAL    END-DIVIDE.
 
-      * TIEMPO EN AÑOS
-           DISPLAY 'TIEMPO EN ANOS:'     LINE 18 POSITION 01
-           ACCEPT WS-ANO-TOT             LINE 18 POSITION 21
+       2005-03-CAPTURA-TIEMPO.
+           DISPLAY 'TIEMPO EN ANOS:'     LINE 17 POSITION 01
+           ACCEPT WS-ANO-TOT             LINE 17 POSITION 21.
 
-      * GENERO DEL USUARIO     
+       2005-04-CAPTURA-SEXO.
            DISPLAY 'SELECCIONE SU GENERO:'
                                          LINE 19 POSITION 01
            DISPLAY 'H/h) HOMBRE - M/m) MUJER' 
@@ -233,7 +216,7 @@
            DISPLAY 'OPCION) '            LINE 21 POSITION 01
            ACCEPT WS-GENERO              LINE 21 POSITION 09.
 
-      * CABEZA DE HOGAR
+       2005-05-CAPTURA-HOGAR.
            DISPLAY 'CABEZA DE HOGAR? S-s) SI / N-n) NO: ' 
                                          LINE 23 POSITION 01
            DISPLAY 'OPCION) '            LINE 24 POSITION 01
@@ -260,7 +243,7 @@
                     SET WS-INT-EDU TO TRUE
                     SET WS-PRO-EDU TO TRUE
                   ELSE
-                    PERFORM 2009-OPCION-NO-ENCONTRADA
+                    PERFORM 2017-OPCION-NO-ENCONTRADA
                   END-IF
                 END-IF
                END-IF
@@ -270,7 +253,7 @@
        2007-HALLAR-INTERESES.
           *>  INTERESES= TASA DE INTERES - DESCUENTOS
            SUBTRACT WS-DESCUENTO FROM WS-INTERES ROUNDED
-               ON SIZE ERROR PERFORM 2009-OPCION-NO-ENCONTRADA
+               ON SIZE ERROR PERFORM 2017-OPCION-NO-ENCONTRADA
                NOT ON SIZE ERROR
                MULTIPLY 100 BY WS-INTERES GIVING WS-INTERES-PAN ROUNDED
                MOVE WS-INTERES-PAN TO WS-MAS-INTERES
@@ -280,10 +263,8 @@
            IF WS-HOGAR = 'S' OR 's' THEN
               MOVE 'SI' TO WS-HOGAR
               IF WS-GENERO = 'H' OR 'h' THEN
-                MOVE 'HOMBRE' TO WS-GEN-SEL
                 SET WS-DST-HOM TO TRUE
               ELSE
-                MOVE 'MUJER' TO WS-GEN-SEL
                 SET WS-DST-MUJ TO TRUE
               END-IF
            ELSE
@@ -291,133 +272,164 @@
                 SET WS-DST-NO TO TRUE
            END-IF.
 
-       2011-HALLAR-SEGURO-MENSUAL.
+       2009-HALLAR-SEXO.
+           IF WS-GENERO = 'H' OR 'h' THEN
+               MOVE 'HOMBRE' TO WS-GEN-SEL
+           ELSE
+               IF WS-GENERO = 'M' OR 'm' THEN
+                   MOVE 'MUJER' TO WS-GEN-SEL
+               ELSE
+                   PERFORM 2017-OPCION-NO-ENCONTRADA
+               END-IF
+           END-IF.
+
+       2010-HALLAR-SEGURO-MENSUAL.
            COMPUTE WS-SEG-TOT ROUNDED = WS-CAPITAL * ( WS-SEGURO / 12 )
-               ON SIZE ERROR PERFORM 2009-OPCION-NO-ENCONTRADA
-               NOT ON SIZE ERROR MOVE WS-SEG-TOT TO WS-SEG-TOT-MAS
+               ON SIZE ERROR PERFORM 2017-OPCION-NO-ENCONTRADA
            END-COMPUTE.
 
-       2013-HALLAR-INTERESES-MENSUALES.
+       2011-HALLAR-INTERESES-MENSUALES.
            COMPUTE WS-MES-TOT ROUNDED = WS-CAPITAL * (WS-INTERES
                / 12) * (WS-ANO-TOT / 12)
-               ON SIZE ERROR PERFORM 2009-OPCION-NO-ENCONTRADA
-               NOT ON SIZE ERROR MOVE WS-MES-TOT TO WS-MES-TOT-MAS
+               ON SIZE ERROR PERFORM 2017-OPCION-NO-ENCONTRADA
            END-COMPUTE. 
 
-       2014-HALLAR-TOTAL.
+       2012-HALLAR-TOTAL.
            MULTIPLY WS-CUOTAS BY WS-CUOTA-MEN GIVING WS-TOTAL ROUNDED
-           END-MULTIPLY
-           MOVE WS-TOTAL TO WS-MAS-TOT.
+           END-MULTIPLY.
 
-       2015-HALLAR-MENSUALES-TOTALES.
+       2013-HALLAR-MENSUALES-TOTALES.
            COMPUTE WS-SEGURO-TOT ROUNDED = WS-SEG-TOT * 
            (WS-ANO-TOT * 12)
-               ON SIZE ERROR PERFORM 2009-OPCION-NO-ENCONTRADA
-               NOT ON SIZE ERROR MOVE WS-SEGURO-TOT TO WS-SEGURO-MAS
+               ON SIZE ERROR PERFORM 2017-OPCION-NO-ENCONTRADA
            END-COMPUTE
            COMPUTE WS-INTERES-TOT ROUNDED = WS-MES-TOT * 
            (WS-ANO-TOT * 12)
-               ON SIZE ERROR PERFORM 2009-OPCION-NO-ENCONTRADA
-               NOT ON SIZE ERROR MOVE WS-INTERES-TOT TO WS-INTERES-MAS
+               ON SIZE ERROR PERFORM 2017-OPCION-NO-ENCONTRADA
            END-COMPUTE.
 
-       2016-HALLAR-CAPITAL-MENSUAL.
+       2014-HALLAR-CAPITAL-MENSUAL.
       *    CAPITAL / MESES,      
            MULTIPLY WS-ANO-TOT BY 12 GIVING WS-CUOTAS ROUNDED 
            END-MULTIPLY
            DIVIDE WS-CUOTAS INTO WS-CAPITAL GIVING WS-CAP-MES ROUNDED
-               ON SIZE ERROR PERFORM 2009-OPCION-NO-ENCONTRADA
-               NOT ON SIZE ERROR MOVE WS-CAP-MES TO WS-CAPITAL-MAS
+               ON SIZE ERROR PERFORM 2017-OPCION-NO-ENCONTRADA
            END-DIVIDE.
 
-       2017-HALLAR-CUOTA-MENSUAL.
+       2015-HALLAR-CUOTA-MENSUAL.
       *     SEGURO + INTERES + CAPITAL
            ADD WS-CAP-MES WS-MES-TOT WS-SEG-TOT GIVING WS-CUOTA-MEN
                ROUNDED
-               ON SIZE ERROR PERFORM 2009-OPCION-NO-ENCONTRADA
-               NOT ON SIZE ERROR MOVE WS-CUOTA-MEN TO WS-CUOTAS-MAS
+               ON SIZE ERROR PERFORM 2017-OPCION-NO-ENCONTRADA
+               NOT ON SIZE ERROR MOVE WS-CUOTA-MEN TO WS-MAS-DINERO
            END-ADD.
 
-       2019-SALIDA.
+       2016-SALIDA.
            DISPLAY CLEAR-SCREEN
            PERFORM 2002-PANTALLA-FECHAS
            PERFORM 2003-BANNER
-      * CAPITAL
+           PERFORM 2016-01-SALIDA-CAPITAL
+           PERFORM 2016-02-SALIDA-ANOS
+           PERFORM 2016-03-SALIDA-PRODUCTO
+           PERFORM 2016-04-SALIDA-GENERO
+           PERFORM 2016-05-SALIDA-HOGAR
+           PERFORM 2016-06-SALIDA-PORCENTAJE-INT
+           PERFORM 2016-07-SALIDA-SEGURO
+           PERFORM 2016-08-RESULTADO-SEGURO-MEN
+           PERFORM 2016-09-RESULTADO-INTERES-MEN
+           PERFORM 2016-10-RESULTADO-CAPITAL-MEN
+           PERFORM 2016-11-RESULTADO-TOTAL-MEN
+           PERFORM 2016-12-RESULTADO-SEGURO-TOT
+           PERFORM 2016-13-RESULTADO-INTERES-TOT
+           PERFORM 2016-14-TOTAL-PAGAR.
+
+       2016-01-SALIDA-CAPITAL.
            DISPLAY 'CAPITAL:'            LINE 07 POSITION 01 
            MOVE WS-CAPITAL               TO WS-MAS-DINERO
-           DISPLAY WS-MAS-DINERO         LINE 07 POSITION 25
+           DISPLAY WS-MAS-DINERO         LINE 07 POSITION 25.
 
-      * ANOS 
+       2016-02-SALIDA-ANOS.
            DISPLAY 'TIEMPO A PAGAR EN ANOS:' 
                                          LINE 08 POSITION 01
            MOVE WS-ANO-TOT TO WS-ANO-MAS
-           DISPLAY WS-ANO-MAS            LINE 08 POSITION 25
+           DISPLAY WS-ANO-MAS            LINE 08 POSITION 25.
 
-      * PRODUCTOS     
+       2016-03-SALIDA-PRODUCTO.
            DISPLAY 'PRODUCTO SELECCIONADO:'
                                          LINE 09 POSITION 01
-           DISPLAY WS-PRO-SEL            LINE 09 POSITION 25
+           DISPLAY WS-PRO-SEL            LINE 09 POSITION 25.
 
-      * GENERO
+       2016-04-SALIDA-GENERO.
            DISPLAY 'GENERO:'             LINE 10 POSITION 01
-           DISPLAY WS-GEN-SEL            LINE 10 POSITION 25
+           DISPLAY WS-GEN-SEL            LINE 10 POSITION 25.
 
-      * CABEZA DE HOGAR 
+       2016-05-SALIDA-HOGAR.
            DISPLAY 'CABEZA DE HOGAR:'    LINE 11 POSITION 01
-           DISPLAY WS-HOGAR              LINE 11 POSITION 25
+           DISPLAY WS-HOGAR              LINE 11 POSITION 25.
 
-      * PORCENTAJE INTERES     
+       2016-06-SALIDA-PORCENTAJE-INT.
            DISPLAY 'PORCENTAJE INTERES:' LINE 12 POSITION 01
            DISPLAY '%'                   LINE 12 POSITION 30
-           DISPLAY WS-MAS-INTERES        LINE 12 POSITION 25
+           DISPLAY WS-MAS-INTERES        LINE 12 POSITION 25.
 
-      * SEGURO
+       2016-07-SALIDA-SEGURO.
            DISPLAY 'SEGURO:'             LINE 13 POSITION 01
            MULTIPLY 100 BY WS-SEGURO GIVING WS-SEGURO-PAN END-MULTIPLY
-           MOVE WS-SEGURO-PAN            TO WS-MAS-SEG
-           DISPLAY WS-MAS-SEG            LINE 13 POSITION 25
-           DISPLAY '%'                   LINE 13 POSITION 28
+           MOVE WS-SEGURO-PAN            TO WS-MAS-DINERO
+           DISPLAY WS-MAS-DINERO         LINE 13 POSITION 25
+           DISPLAY '%'                   LINE 13 POSITION 47.
 
-      * RESULTADOS
-      * SEGURO MENSUAL
+       2016-08-RESULTADO-SEGURO-MEN.
            DISPLAY 'RESULTADOS:'         LINE 15 POSITION 01
            DISPLAY 'SEGURO MENSUAL:'     LINE 16 POSITION 01
-           DISPLAY WS-SEG-TOT-MAS        LINE 16 POSITION 25
+           MOVE WS-SEG-TOT               TO WS-MAS-DINERO
+           DISPLAY WS-MAS-DINERO         LINE 16 POSITION 25.
 
       * INTERES MENSUAL
+       2016-09-RESULTADO-INTERES-MEN.
            DISPLAY 'INTERES MENSUAL:'    LINE 17 POSITION 01
-           DISPLAY WS-MES-TOT-MAS        LINE 17 POSITION 25
+           MOVE WS-MES-TOT              TO WS-MAS-DINERO
+           DISPLAY WS-MAS-DINERO         LINE 17 POSITION 25.
 
       * CAPITAL MENSUAL     
+       2016-10-RESULTADO-CAPITAL-MEN.
            DISPLAY 'CAPITAL MENSUAL:'    LINE 18 POSITION 01
-           DISPLAY WS-CAPITAL-MAS        LINE 18 POSITION 25
+           MOVE  WS-CAP-MES              TO WS-MAS-DINERO
+           DISPLAY WS-MAS-DINERO         LINE 18 POSITION 25.
 
       * VALOR TOTAL MENSUAL     
+       2016-11-RESULTADO-TOTAL-MEN.
            DISPLAY 'VALOR MENSUAL: '     LINE 19 POSITION 01
-           DISPLAY WS-CUOTAS-MAS         LINE 19 POSITION 25
+           MOVE WS-CUOTA-MEN             TO WS-MAS-DINERO
+           DISPLAY WS-MAS-DINERO         LINE 19 POSITION 25.
            
       * VALOR SEGURO TOTAL     
+       2016-12-RESULTADO-SEGURO-TOT.
            DISPLAY 'SEGURO TOTAL:'       LINE 21 POSITION 01
-           DISPLAY WS-SEGURO-MAS         LINE 21 POSITION 25
+           MOVE  WS-SEGURO-TOT           TO WS-MAS-DINERO
+           DISPLAY WS-MAS-DINERO         LINE 21 POSITION 25.
 
-      * VALOR INTERES TOTAL
+       2016-13-RESULTADO-INTERES-TOT. 
            DISPLAY 'INTERES TOTAL:'      LINE 22 POSITION 01
-           DISPLAY WS-INTERES-MAS        LINE 22 POSITION 25
+           MOVE WS-INTERES-TOT           TO WS-MAS-DINERO
+           DISPLAY WS-MAS-DINERO         LINE 22 POSITION 25.
 
       * TOTAL A PAGAR
+       2016-14-TOTAL-PAGAR.
            DISPLAY 'TOTAL A PAGAR:'      LINE 23 POSITION 01
-           DISPLAY WS-MAS-TOT            LINE 23 POSITION 25.
+           MOVE WS-TOTAL                 TO WS-MAS-DINERO
+           DISPLAY WS-MAS-DINERO         LINE 23 POSITION 25.
 
-       2009-OPCION-NO-ENCONTRADA.
+       2017-OPCION-NO-ENCONTRADA.
            DISPLAY CLEAR-SCREEN
            PERFORM 2002-PANTALLA-FECHAS
            PERFORM 2003-BANNER
            DISPLAY 'OPCION NO ENCONTRADA'
                                          LINE 12 POSITION 30
-           PERFORM 2020-OPCION
+           PERFORM 2018-OPCION
            PERFORM 3000-FINAL.
 
-       2020-OPCION.
+       2018-OPCION.
            DISPLAY 'PRESIONE UNA TECLA PARA CONTINUAR'
                                          LINE 24 POSITION 23
            ACCEPT WS-OPCION              LINE 24 POSITION 57. 
